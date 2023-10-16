e5687a707dda134d3d5f84cc2523c42c9a57607f
Andrien Montmayeur <92160718+AndrienMont@users.noreply.github.com>
2023-10-16 16:30:43 +0200

    Route post fonctionnelle

--- usmbro/lib/post_users.dart
+++ usmbro/lib/post_users.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:usmbro/main.dart';
+import 'package:usmbro/map.dart';
 
 class PostUsers extends StatefulWidget {
   const PostUsers({super.key, required this.title});
@@ -48,11 +52,14 @@ class _PostUsersState extends State<PostUsers> {
 
   Future<void> createUser(String nom, String prenom, String filiere) async {
     final response = await http.post(
-      Uri.parse("http://127.0.0.1;3000/api/users"),
+      Uri.parse("http://192.168.72.22:3000/api/users/"),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: User(nom: nom, prenom: prenom, filiere: filiere).toJson(),
+      body: jsonEncode(User(
+          nom: nom.toUpperCase(),
+          prenom: prenom,
+          filiere: filiere.toUpperCase())),
     );
     if (response.statusCode == 201) {
       // If the server did return a 201 CREATED response,
@@ -73,100 +80,93 @@ class _PostUsersState extends State<PostUsers> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(
+                          width: 100,
+                          child: TextField(
+                            controller: _controllerNom,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Nom',
+                            ),
+                            onSubmitted: (value) => nomPost = value,
+                          ),
+                        ),
+                        const SizedBox(width: 15),
+                        SizedBox(
+                          width: 100,
+                          child: TextField(
+                            controller: _controllerPrenom,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Prénom',
+                            ),
+                            onSubmitted: (value) => prenomPost = value,
+                          ),
+                        ),
+                        const SizedBox(width: 15),
+                        SizedBox(
+                          width: 100,
+                          child: TextField(
+                            controller: _controllerFiliere,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Filière',
+                            ),
+                            onSubmitted: (value) => filierePost = value,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            createUser(
+                                _controllerNom.text,
+                                _controllerPrenom.text,
+                                _controllerFiliere.text);
+                          });
+                        },
+                        child: const Text("Send to server")),
+                  ],
+                ),
+              ),
               Row(
-                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(
-                    width: 100,
-                    child: TextField(
-                      controller: _controllerNom,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Nom',
-                      ),
-                      onSubmitted: (value) => nomPost = value,
-                    ),
-                  ),
-                  const SizedBox(width: 15),
-                  SizedBox(
-                    width: 100,
-                    child: TextField(
-                      controller: _controllerPrenom,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Prénom',
-                      ),
-                      onSubmitted: (value) => prenomPost = value,
-                    ),
-                  ),
-                  const SizedBox(width: 15),
-                  SizedBox(
-                    width: 100,
-                    child: TextField(
-                      controller: _controllerFiliere,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Filière',
-                      ),
-                      onSubmitted: (value) => filierePost = value,
-                    ),
-                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const MyHomePage(title: "USMBRO Home Page");
+                        }));
+                      },
+                      child: const Text("GET users")),
+                  ElevatedButton(
+                      onPressed: () {}, child: const Text("POST users")),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const MapUsers(title: "Map Users");
+                        }));
+                      },
+                      child: const Text("MAP")),
                 ],
               ),
-              const SizedBox(height: 15),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      createUser(_controllerNom.text, _controllerPrenom.text,
-                          _controllerFiliere.text);
-                    });
-                  },
-                  child: const Text("Send to server"))
+              const SizedBox(
+                height: 20,
+              ),
             ],
           ),
         ));
   }
 }
-
-// class UserTextFields extends StatelessWidget {
-//   const UserTextFields({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Row(
-//       mainAxisSize: MainAxisSize.min,
-//       children: [
-//         SizedBox(
-//           width: 100,
-//           child: TextField(
-//             decoration: InputDecoration(
-//               border: OutlineInputBorder(),
-//               labelText: 'Nom',
-//             ),
-//           ),
-//         ),
-//         SizedBox(width: 15),
-//         SizedBox(
-//           width: 100,
-//           child: TextField(
-//             decoration: InputDecoration(
-//               border: OutlineInputBorder(),
-//               labelText: 'Prénom',
-//             ),
-//           ),
-//         ),
-//         SizedBox(width: 15),
-//         SizedBox(
-//           width: 100,
-//           child: TextField(
-//             decoration: InputDecoration(
-//               border: OutlineInputBorder(),
-//               labelText: 'Filière',
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
