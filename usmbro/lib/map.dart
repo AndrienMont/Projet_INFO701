5affe1233299c0328f385fadcd78beb8875ce682
Emilien39110 <emilien.boitouzet@hotmail.fr>
2023-10-19 20:26:04 +0200

    Ajout de la map dans l'application avec OSM

--- usmbro/lib/map.dart
+++ usmbro/lib/map.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:usmbro/mapController/controlMap.dart';
+//Import OSMFlutter
 
-class MapUsers extends StatelessWidget {
-  const MapUsers({Key? key, required String title}) : super(key: key);
+
+class MapUsers extends StatefulWidget {
+  const MapUsers({super.key, required this.title});
+
+  final String title;
 
   @override
+  State<MapUsers> createState() => _MapUsersState();
+}
+
+
+class _MapUsersState extends State<MapUsers> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Map'),
-      ),
+    // return Scaffold(
+    //   appBar: AppBar(
+    //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    //       title: Text(widget.title),
+    //     ),
+    // );
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+    body: Center(
+      child: SizedBox(
+        width: const SizedBox.expand().width, 
+        height: const SizedBox.expand().height, 
+        child: afficheMap()),
+    ),
     );
   }
 }
+
